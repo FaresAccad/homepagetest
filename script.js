b5fc92ac0c521c2f7cd052ef67a127a63b24e333
FaresAccad <81829773+FaresAccad@users.noreply.github.com>
2024-04-23 20:21:07 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,35 +1,17 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const scrollingContent = document.querySelector('.scrolling-content');
+    let totalContentWidth = 0;
 
-    
-      document.addEventListener('DOMContentLoaded', function() {
-    
-        const scrollingContent = document.querySelector('.scrolling-content');
-    
-        let totalContentWidth = 0;
-    
-        // Consider using getBoundingClientRect which might provide more precise measurement including sub-pixel calculations
-    
-        Array.from(scrollingContent.children).forEach(child => {
-    
-            const childWidth = child.getBoundingClientRect().width;
-    
-            totalContentWidth += childWidth;
-    
-        });
-    
-        const wrapperWidth = document.querySelector('.scrolling-wrapper').getBoundingClientRect().width;
-    
-        // Ensuring the movement is enough to completely hide the content
-    
-        // Adding wrapperWidth ensures that the content has moved completely out of the visible area
-    
-        const buffer = wrapperWidth * 0.0020;
-    
-        const requiredTranslation = totalContentWidth + wrapperWidth + buffer;
-    
-        const translatePercentage = ((-requiredTranslation / wrapperWidth) * 100) / 1.992;
-    
-        document.documentElement.style.setProperty('--translateX-percentage', `${translatePercentage}%`);
-    
-        });
-    
+    Array.from(scrollingContent.children).forEach(child => {
+        const childWidth = child.getBoundingClientRect().width;
+        totalContentWidth += childWidth;
+    });
 
+    const wrapperWidth = document.querySelector('.scrolling-wrapper').getBoundingClientRect().width;
+    const halfContentWidth = totalContentWidth / 2;  // Calculate half the total width of the logos
+    const requiredTranslation = halfContentWidth + wrapperWidth;  // Adjust so it includes the viewport width
+
+    const translatePercentage = (-requiredTranslation / wrapperWidth) * 100;
+
+    document.documentElement.style.setProperty('--translateX-percentage', `${translatePercentage}%`);
+});
