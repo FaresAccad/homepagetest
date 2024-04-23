9927103c59dc678760c05cabbaa9acd1d9c6cdff
FaresAccad <81829773+FaresAccad@users.noreply.github.com>
2024-04-23 20:13:43 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,46 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Coding with Robby</title>
-    <link rel="stylesheet" href="./style.css" />
-  </head>
-  <body>
-    <div class="scrolling-wrapper">
 
-      <div class="scrolling-content">
-    
-        <img src=https://raw.githubusercontent.com/moufidayoubi/Clients-Partners-Logos/main/Eagle_Alpha_Logo.png alt="Client Logo 1">
-    
-        <img src=https://raw.githubusercontent.com/moufidayoubi/Clients-Partners-Logos/main/Kamba%20Logo_Transparent_white%202.png alt="Client Logo 4">
-     
-        <img src=https://raw.githubusercontent.com/moufidayoubi/Clients-Partners-Logos/main/Neudata%20Logo_transparent.png alt="Client Logo 2">
-       <img src=https://raw.githubusercontent.com/moufidayoubi/Clients-Partners-Logos/main/Snowflake%20Logo.png alt="Client Logo 6">
-    
-       <img src=https://raw.githubusercontent.com/moufidayoubi/Clients-Partners-Logos/main/battlefin-logo-horiz.png alt="Client Logo 3" style="height: 8vh; width: auto; top:1.5vh; position: relative;">
-    
-       <img src=https://raw.githubusercontent.com/moufidayoubi/Clients-Partners-Logos/main/png-transparent-databricks-white-logo-tech-companies.png alt="Client Logo 5">
-        
-       <img src=https://raw.githubusercontent.com/moufidayoubi/Clients-Partners-Logos/main/AWS%20FINAL%20LOGO.png alt="Client Logo 7">
-       <img src=https://raw.githubusercontent.com/moufidayoubi/Clients-Partners-Logos/main/Eagle_Alpha_Logo.png alt="Client Logo 1">
-    
-       <img src=https://raw.githubusercontent.com/moufidayoubi/Clients-Partners-Logos/main/Kamba%20Logo_Transparent_white%202.png alt="Client Logo 4">
-    
-       <img src=https://raw.githubusercontent.com/moufidayoubi/Clients-Partners-Logos/main/Neudata%20Logo_transparent.png alt="Client Logo 2">
-      <img src=https://raw.githubusercontent.com/moufidayoubi/Clients-Partners-Logos/main/Snowflake%20Logo.png alt="Client Logo 6">
-   
-      <img src=https://raw.githubusercontent.com/moufidayoubi/Clients-Partners-Logos/main/battlefin-logo-horiz.png alt="Client Logo 3" style="height: 8vh; width: auto; top:1.5vh; position: relative;">
-   
-      <img src=https://raw.githubusercontent.com/moufidayoubi/Clients-Partners-Logos/main/png-transparent-databricks-white-logo-tech-companies.png alt="Client Logo 5">
-       
-      <img src=https://raw.githubusercontent.com/moufidayoubi/Clients-Partners-Logos/main/AWS%20FINAL%20LOGO.png alt="Client Logo 7">
-      </div>
-    
-    </div>
-    
-    <script>
     
       document.addEventListener('DOMContentLoaded', function() {
     
@@ -74,7 +32,4 @@
     
         });
     
-    </script>
-  </body>
 
-</html>
