c09e1696e3d532abe869777c780ad76ded4502bf
FaresAccad <81829773+FaresAccad@users.noreply.github.com>
2024-04-24 13:58:38 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,23 @@
 document.addEventListener('DOMContentLoaded', function() {
     const scrollingContent = document.querySelector('.scrolling-content');
-    let totalContentWidth = 0;
-
-    Array.from(scrollingContent.children).forEach(child => {
-        const childWidth = child.getBoundingClientRect().width;
-        totalContentWidth += childWidth;
-    });
-
     const wrapperWidth = document.querySelector('.scrolling-wrapper').getBoundingClientRect().width;
-    const halfContentWidth = totalContentWidth / 2;  // Calculate half the total width of the logos
-    const requiredTranslation = halfContentWidth + wrapperWidth;  // Adjust so it includes the viewport width
 
-    const translatePercentage = (-requiredTranslation / wrapperWidth) * 100;
+    function updateAnimation() {
+        let totalContentWidth = 0;
 
-    document.documentElement.style.setProperty('--translateX-percentage', `${translatePercentage}%`);
+        Array.from(scrollingContent.children).forEach(child => {
+            const childWidth = child.getBoundingClientRect().width;
+            totalContentWidth += childWidth;
+        });
+
+        const buffer = wrapperWidth * 0.0020;
+        const requiredTranslation = (totalContentWidth / 2) + wrapperWidth + buffer;
+        const translatePercentage = ((-requiredTranslation / wrapperWidth) * 100);
+
+        document.documentElement.style.setProperty('--translateX-percentage', `${translatePercentage}%`);
+    }
+
+    updateAnimation(); // Call on initial load
+
+    window.addEventListener('resize', updateAnimation); // Adjust on window resize
 });
