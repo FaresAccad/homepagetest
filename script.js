a3fc48ebed0d629449f7228e7a82e0a27149ce13
FaresAccad <81829773+FaresAccad@users.noreply.github.com>
2024-04-24 14:12:06 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
     const scrollingContent = document.querySelector('.scrolling-content');
-    const wrapperWidth = document.querySelector('.scrolling-wrapper').getBoundingClientRect().width;
+    const wrapper = document.querySelector('.scrolling-wrapper');
 
     function updateAnimation() {
         let totalContentWidth = 0;
@@ -10,14 +10,22 @@ document.addEventListener('DOMContentLoaded', function() {
             totalContentWidth += childWidth;
         });
 
+        const wrapperWidth = wrapper.getBoundingClientRect().width;
         const buffer = wrapperWidth * 0.0020;
-        const requiredTranslation = (totalContentWidth / 4) + wrapperWidth + buffer;
+        const requiredTranslation = (totalContentWidth / 2) + wrapperWidth + buffer;
         const translatePercentage = ((-requiredTranslation / wrapperWidth) * 100);
 
         document.documentElement.style.setProperty('--translateX-percentage', `${translatePercentage}%`);
+        restartAnimation();
+    }
+
+    function restartAnimation() {
+        scrollingContent.classList.remove('animate-slide');
+        setTimeout(() => {
+            scrollingContent.classList.add('animate-slide');
+        }, 50); // Wait 50 milliseconds before restarting the animation
     }
 
     updateAnimation(); // Call on initial load
-
     window.addEventListener('resize', updateAnimation); // Adjust on window resize
-});
+    });
